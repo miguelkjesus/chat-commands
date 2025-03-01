7dd39933b2445b591520b7110aea0382882b5f0e
miguelkjesus <miguelkjesus@gmail.com>
2025-03-01 12:49:44 +0000

    Unnecessary defaults

--- src/builders/parameters/number-builder.ts
+++ src/builders/parameters/number-builder.ts
@@ -1,6 +1,4 @@
 import type { NumberParameter } from "~/parameters";
-import { NumberRange } from "~/utils/range";
-
 import { ParameterBuilder } from "./parameter-builder";
 
 export class NumberParameterBuilder<Name extends string> extends ParameterBuilder<NumberParameter<Name>> {
@@ -13,22 +11,18 @@ export class NumberParameterBuilder<Name extends string> extends ParameterBuilde
   }
 
   gt(gt: number) {
-    this.__default({ range: new NumberRange({}) });
     return this.__mutate(({ range }) => (range!.min = { inclusive: false, value: gt }));
   }
 
   gte(gte: number) {
-    this.__default({ range: new NumberRange({}) });
     return this.__mutate(({ range }) => (range!.min = { inclusive: true, value: gte }));
   }
 
   lt(lt: number) {
-    this.__default({ range: new NumberRange({}) });
     return this.__mutate(({ range }) => (range!.max = { inclusive: false, value: lt }));
   }
 
   lte(lte: number) {
-    this.__default({ range: new NumberRange({}) });
     return this.__mutate(({ range }) => (range!.max = { inclusive: true, value: lte }));
   }
 }
