63f506a0ea68b508fd2dba5f69a7e60d001aa726
miguelkjesus <miguelkjesus@gmail.com>
2025-03-04 15:33:38 +0000

    Change bundle file name

--- tsup.config.js
+++ tsup.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: { commands: `src/index.ts` },
+  entry: { "chat-commands": `src/index.ts` },
   outDir: "dist/bundle",
 });
