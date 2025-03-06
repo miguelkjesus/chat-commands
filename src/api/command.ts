48c254da2d136dd82816c8efa28a810a01991d13
miguelkjesus <miguelkjesus@gmail.com>
2025-03-06 11:52:59 +0000

    Validate command names to prevent multiple-token names

--- src/api/command.ts
+++ src/api/command.ts
@@ -2,9 +2,15 @@ import { CommandBuilder } from "~/builders";
 import { Command } from "~/commands";
 
 import { manager } from "./command-manager";
+import { tokenize } from "~/tokens";
 
 export function command(name: string, ...aliases: string[]) {
-  // TODO, what to do about multiple token names?
+  const tokens = tokenize(name);
+  if (tokens.length > 1) {
+    throw new Error("Command names must have no spaces!");
+    // TODO: Potentially avoid this by using command names directly in the tokenizer?
+    // e.g. commands = [help, foo bar, teleport], it would try match each string in its entirety before just matching a string
+  }
 
   const builder = new CommandBuilder(new Command(name, aliases, []));
   manager.commands.add(builder.__state);
