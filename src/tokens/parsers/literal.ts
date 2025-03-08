57783f776c65f704df87914b9becc12ea536f595
miguelkjesus <miguelkjesus@gmail.com>
2025-03-08 21:16:09 +0000

    Fix literal parser

--- src/tokens/parsers/literal.ts
+++ src/tokens/parsers/literal.ts
@@ -3,12 +3,12 @@ import { argument } from "./argument";
 
 export function literal(...choices: string[]): TokenParser<string | undefined> {
   return (unparsed: string) => {
-    const matches = choices.filter((choice) => unparsed.startsWith(choice));
-    const longest = matches.reduce((a, b) => (a.length > b.length ? a : b));
+    const matches = choices.filter((choice) => unparsed.startsWith(choice + " "));
+    const longest = matches.reduce<string>((a, b) => (a.length > b.length ? a : b), "");
 
     if (longest) {
       return {
-        unparsed: unparsed.slice(longest.length),
+        unparsed: unparsed.slice(longest.length + 1),
         token: longest,
       };
     }
