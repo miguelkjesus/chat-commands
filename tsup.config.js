850dd478353efe99f8dc4b565c43539527bb410f
miguelkjesus <miguelkjesus@gmail.com>
2025-03-13 11:23:56 +0000

    Add minified bundle and bundle watching

--- tsup.config.js
+++ tsup.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: { "chat-commands": `src/index.ts` },
+/** @type {import("tsup").Options} */
+const shared = {
   outDir: "dist/bundle",
-});
+  sourcemap: true,
+};
+
+export default defineConfig([
+  {
+    entry: { "chat-commands": `src/index.ts` },
+    dts: true,
+    ...shared,
+  },
+  {
+    entry: { "chat-commands.min": `src/index.ts` },
+    minify: true,
+    ...shared,
+  },
+]);
