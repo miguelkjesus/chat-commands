dd487f83f0b274e91ac1c65052ecdbfc054c62e1
miguelkjesus <miguelkjesus@gmail.com>
2025-03-20 20:52:22 +0000

    Add documentation for overload builder methods

--- src/builders/overload-builder.ts
+++ src/builders/overload-builder.ts
@@ -3,12 +3,46 @@ import type { InvocationCallback, Overload } from "~/commands";
 import type { ParameterBuilder, ParametersFromBuilders } from "./parameter-types";
 import { Builder } from "./builder";
 
+/**
+ * A builder for creating and configuring command overloads.
+ * Overloads define different parameter patterns a command can accept.
+ *
+ * @template T
+ *    The overload definition being built.
+ */
 export class OverloadBuilder<T extends Overload = Overload> extends Builder<T> {
+  /**
+   * Defines what this overload should do once it has been triggered.
+   *
+   * @example
+   * overload({ amount: number() }).onExecute((ctx, { amount }) => {
+   *   ctx.player.setDynamicProperty("money", amount);
+   * });
+   *
+   * @param execute
+   *    The function that will be called when this overload matches.
+   * @returns
+   *    The overload builder instance.
+   */
   onExecute(execute: InvocationCallback<T>) {
     this.state.execute = execute;
     return this;
   }
 
+  /**
+   * Sets a description for the overload.
+   * This description is typically displayed in help menus.
+   *
+   * @example
+   * command("give money")
+   *   .createOverload({ amount: number() })
+   *   .setDescription("Give yourself money.");
+   *
+   * @param description
+   *    A brief explanation of what this overload does.
+   * @returns
+   *    The overload builder instance.
+   */
   setDescription(description: string) {
     this.state.description = description;
     return this;
