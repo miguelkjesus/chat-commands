7d7b1e8844ca4c4f8fa028420bbaf13020a384e7
miguelkjesus <miguelkjesus@gmail.com>
2025-04-18 15:32:12 +0100

    Fix cooldown manager memory leak and add clear method

--- src/commands/cooldown-manager.ts
+++ src/commands/cooldown-manager.ts
@@ -1,23 +1,57 @@
-import { system, world } from "@minecraft/server";
+import { system } from "@minecraft/server";
 
 export class CooldownManager {
-  cooldownTicks: number;
-  playerTriggeredAtTicks = new Map<string, number>();
+  private triggers = new Map<string, { triggeredAtTick: number; deleteTriggerTimeoutId: number }>();
+  private _cooldownTicks: number;
 
   constructor(cooldownTicks = 0) {
     this.cooldownTicks = cooldownTicks;
-
-    world.afterEvents.playerLeave.subscribe((event) => {
-      this.playerTriggeredAtTicks.delete(event.playerId);
-    });
   }
 
-  getRemainingTicks(playerId: string): number {
-    const lastExecutedTick = this.playerTriggeredAtTicks.get(playerId) ?? 0;
+  get cooldownTicks() {
+    return this._cooldownTicks;
+  }
+
+  set cooldownTicks(value: number) {
+    this._cooldownTicks = value;
+
+    for (const [entityId, info] of this.triggers.entries()) {
+      system.clearRun(info.deleteTriggerTimeoutId);
+
+      info.deleteTriggerTimeoutId = system.runTimeout(
+        () => {
+          this.triggers.delete(entityId);
+        },
+        this._cooldownTicks - system.currentTick - info.triggeredAtTick,
+      );
+    }
+  }
+
+  getRemainingTicks(entityId: string) {
+    const lastExecutedTick = this.triggers.get(entityId)?.triggeredAtTick ?? 0;
     return Math.max(0, this.cooldownTicks - (system.currentTick - lastExecutedTick));
   }
 
-  trigger(playerId: string): void {
-    this.playerTriggeredAtTicks.set(playerId, system.currentTick);
+  clear(entityId: string) {
+    const runId = this.triggers.get(entityId)?.deleteTriggerTimeoutId;
+
+    if (runId) {
+      system.clearRun(runId);
+    }
+
+    this.triggers.delete(entityId);
+  }
+
+  trigger(entityId: string): void {
+    this.clear(entityId);
+
+    const runId = system.runTimeout(() => {
+      this.triggers.delete(entityId);
+    }, this.cooldownTicks);
+
+    this.triggers.set(entityId, {
+      triggeredAtTick: system.currentTick,
+      deleteTriggerTimeoutId: runId,
+    });
   }
 }
